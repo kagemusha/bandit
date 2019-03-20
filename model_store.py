c678ad7e369e706ef9f5c7edf02c8faa5fc51b63
kagemusha <mmadrid99@gmail.com>
2019-03-20 16:24:07 -0400

    model persistence using pickle

---
+++ model_store.py
@@ -0,0 +1,24 @@
+import pickle
+
+MODEL_PREFIX = "_htmodel"
+
+def get_model_filename(choice):
+  return f"{MODEL_PREFIX}_{choice}"
+
+def save_model_file(choice, model):
+  filename = get_model_filename(choice)
+  print("pickling ", filename)
+  pickle_model(filename, model)
+
+def pickle_model(filename, model):
+  outfile = open(filename, 'wb')
+  pickle.dump(model, outfile)
+  outfile.close()
+
+def load_model_file(choice):
+  filename = get_model_filename(choice)
+  print("unpickling ", filename)
+  infile = open(filename, 'rb')
+  model = pickle.load(infile)
+  infile.close()
+  return model
